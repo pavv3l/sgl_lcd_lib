ba5bf32f20054bb1b6c55dea616f1f5955e78dab
nte <pavv3l@gmail.com>
2022-11-18 22:22:43 +0100

    add implementation of the write line function

--- sgl/inc/sgl.h
+++ sgl/inc/sgl.h
@@ -2,6 +2,7 @@
 #define __SGL_H__
 
 #inlcude <stdint.h>
+#include <utility>
 //#include <cstdint> // c++ version of stdint.h
 
 // macros definitions for color converting
@@ -26,49 +27,53 @@ namespace sgl
         hole    = 0x01 // color WHITE for BW displays
     }
 
-    struct Coord
-    {
-        Coord(uint16_t xx, uint16_t yy) : x(xx), y(yy) {}
+    //struct Coord
+    //{
+    //    Coord(uint16_t xx, uint16_t yy) : x(xx), y(yy) {}
 
-        uint16_t x = 0;
-        uint16_t y = 0;
-    }
+    //      uint16_t x = 0;
+    //    uint16_t y = 0;
+    //};
 
 
     class SGL
     {
     public:
         virtual void drawPixel(const uint16_t x, const uint16_t y, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd) = 0;
-        virtual void drawPixel(const Coord& coord, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd);
+        //virtual void drawPixel(const Coord& coord, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd) = 0;
         
         virtual void drawLine(const uint16_t x0, const uint16_t y0, const uint16_t x1, const uint16_t y1,
             const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd);
         // line with length 0 == nothing, line with length 1 == one pixel
-        virtual void drawLine(const Coord& coord0, const Coord& coord1, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd);
+        //virtual void drawLine(const Coord& coord0, const Coord& coord1, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd);
         
         // for faster drawing you should override these functions (horizonatal and vertical line)
         virtual void drawHorizontalLine(const uint16_t x0, const uint16_t y0, const uint16_t x1, const uint16_t y1,
             const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd)
-        virtual void drawHorizontalLine(const Coord& coord0, const Coord& coord1, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd);
+        //virtual void drawHorizontalLine(const Coord& coord0, const Coord& coord1, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd);
         
         virtual void drawVerticalLine(const uint16_t x0, const uint16_t y0, const uint16_t x1, const uint16_t y1,
             const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd)
-        virtual void drawVerticalLine(const Coord& coord0, const Coord& coord1, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd);
+        //virtual void drawVerticalLine(const Coord& coord0, const Coord& coord1, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd);
 
         virtual void drawRectangle(const uint16_t x0, const uint16_t y0, const uint16_t x1, const uint16_t y1,
             const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
-        virtual void drawRectangle(const Coord& coord0, const Coord& coord1,
-            const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
+        //virtual void drawRectangle(const Coord& coord0, const Coord& coord1,
+        //    const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
         
         virtual void drawTriangle(const uint16_t x0, const uint16_t y0, const uint16_t x1, const uint16_t y1,
             const uint16_t x2, const uint16_t y2, const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
-        virtual void drawTriangle(const Coord& coord0, const Coord& coord1, const Coord& coord2,
-            const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
+        //virtual void drawTriangle(const Coord& coord0, const Coord& coord1, const Coord& coord2,
+        //    const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
         
         virtual void drawCircle(const uint16_t x0, const uint16_t y0, const uint16_t radius,
             const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
-        virtual void drawCircle(const Coord& coord, const uint16_t radius,
-            const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
+        //virtual void drawCircle(const Coord& coord, const uint16_t radius,
+        //    const uint16_t color = WHITE, const Fill fill = Fill:hole, const Mode mode = Mode::pixelAnd);
+
+    protected:
+        uint16_t _width;
+        uint16_t _height;
     };
 
 }
--- sgl/src/sgl.cpp
+++ sgl/src/sgl.cpp
@@ -1,1 +1,66 @@
 #include "sgl.h"
+
+// you shuld use it only if you have enough memory
+#define USE_FRAME_BUFFER
+// framerate should be adjusted to the display size and the communication speed with the mcu
+#define FRAMERATE 25
+
+void SGL::drawLine(onst uint16_t x0, const uint16_t y0, const uint16_t x1, const uint16_t y1, const uint16_t color = WHITE, const Mode mode = Mode::pixelAnd)
+{
+    if(x0 >= _width)
+        x0 = _width - 1;
+    if(x1 >= _width)
+        x1 = _width - 1;
+    if(y0 >= _height)
+        y0 = _height - 1;
+    if(y1 >= _height)
+        y1 = _height - 1;
+
+    uint16_t dx = std::abs(x0 - x1);
+    uint16_t dy = std::abs(y0 - y1);
+
+    if(dx == 0)
+    {
+        drawHorizontalLine(coord0, coord1, color, mode);
+    }
+    if(dy == 0)
+    {
+        drawVerticalLine(coord0, coord1, color, mode);
+    }
+
+    // signs of x and y axes
+    int8_t x_mult = (x0 > x1) ? -1 : 1;
+    int8_t y_mult = (y0 > y1) ? -1 : 1;
+
+    // positive slope
+    if(dy < dx)
+    {
+        int16_t d = (2 * dy) - dx;
+        uint16_t y = 0;
+        for(uint16_t x = 0; x <= dx; ++x)
+        {
+            drawPixel(x0 + (x * x_mult), y0 + (y * y_mult), color, mode);
+            if(d > 0)
+            {
+                ++y;
+                d -= dx;
+            }
+            d += dy;
+        }
+    }
+    else // negative slope
+    {
+        int16_t d = (2 * dx) - dy;
+        uint16_t x = 0;
+        for(uint16_t y = 0; y <= dy; ++y)
+        {
+            drawPixel(x0 + (x * x_mult), y0 + (y * y_mult), color, mode);
+            if(d > 0)
+            {
+                ++x;
+                d -= dy;
+            }
+            d += dx;
+        }
+    }
+}
