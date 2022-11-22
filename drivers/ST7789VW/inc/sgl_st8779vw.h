5508314d0c91cb60b563bffb9cdcb488d9cc9923
nte <pavv3l@gmail.com>
2022-11-22 21:20:45 +0100

    add init and write data/command functions to driver

--- drivers/ST7789VW/inc/sgl_st8779vw.h
+++ drivers/ST7789VW/inc/sgl_st8779vw.h
@@ -15,7 +15,7 @@
 //  RST     GP12    Reset pin, low active
 //  BL      GP13    Backlight control
 //  A       GP15    User button A
-//  B	    GP17    User button B
+//  B       GP17    User button B
 //  UP      GP2     Joystick-up
 //  DOWM    GP18    Joystick-down
 //  LEFT    GP16    Joystick-left
@@ -34,158 +34,6 @@
 //  135x240     48,6    64,8
 //  240x240     86,4    115,2
 //  240x320     115,2   153,6
-
-/*
-void LCD_1IN14_Init(UBYTE Scan_dir)
-{
-    DEV_SET_PWM(90);
-    //Hardware reset
-    LCD_1IN14_Reset();
-
-    //Set the resolution and scanning method of the screen
-    LCD_1IN14_SetAttributes(Scan_dir);
-    
-    //Set the initialization register
-    LCD_1IN14_InitReg();
-}
-
-static void LCD_1IN14_Reset(void)
-{
-    DEV_Digital_Write(EPD_RST_PIN, 1);
-    DEV_Delay_ms(100);
-    DEV_Digital_Write(EPD_RST_PIN, 0);
-    DEV_Delay_ms(100);
-    DEV_Digital_Write(EPD_RST_PIN, 1);
-    DEV_Delay_ms(100);
-}
-
-static void LCD_1IN14_SetAttributes(UBYTE Scan_dir)
-{
-    //Get the screen scan direction
-    LCD_1IN14.SCAN_DIR = Scan_dir;
-    UBYTE MemoryAccessReg = 0x00;
-
-    //Get GRAM and LCD width and height
-    if(Scan_dir == HORIZONTAL) {
-        LCD_1IN14.HEIGHT	= LCD_1IN14_WIDTH;
-        LCD_1IN14.WIDTH   = LCD_1IN14_HEIGHT;
-        MemoryAccessReg = 0X70;
-    } else {
-        LCD_1IN14.HEIGHT	= LCD_1IN14_HEIGHT;       
-        LCD_1IN14.WIDTH   = LCD_1IN14_WIDTH;
-        MemoryAccessReg = 0X00;
-    }
-
-    // Set the read / write scan direction of the frame memory
-    LCD_1IN14_SendCommand(0x36); //MX, MY, RGB mode
-    LCD_1IN14_SendData_8Bit(MemoryAccessReg);	//0x08 set RGB
-}
-
-    // Set the read / write scan direction of the frame memory
-    LCD_1IN14_SendCommand(0x36); //MX, MY, RGB mode
-    LCD_1IN14_SendData_8Bit(MemoryAccessReg);	//0x08 set RGB
-
-    static void LCD_1IN14_InitReg(void)
-{
-    LCD_1IN14_SendCommand(0x3A);
-    LCD_1IN14_SendData_8Bit(0x05);
-
-    LCD_1IN14_SendCommand(0xB2);
-    LCD_1IN14_SendData_8Bit(0x0C);
-    LCD_1IN14_SendData_8Bit(0x0C);
-    LCD_1IN14_SendData_8Bit(0x00);
-    LCD_1IN14_SendData_8Bit(0x33);
-    LCD_1IN14_SendData_8Bit(0x33);
-
-    LCD_1IN14_SendCommand(0xB7);  //Gate Control
-    LCD_1IN14_SendData_8Bit(0x35);
-
-    LCD_1IN14_SendCommand(0xBB);  //VCOM Setting
-    LCD_1IN14_SendData_8Bit(0x19);
-
-    LCD_1IN14_SendCommand(0xC0); //LCM Control     
-    LCD_1IN14_SendData_8Bit(0x2C);
-
-    LCD_1IN14_SendCommand(0xC2);  //VDV and VRH Command Enable
-    LCD_1IN14_SendData_8Bit(0x01);
-    LCD_1IN14_SendCommand(0xC3);  //VRH Set
-    LCD_1IN14_SendData_8Bit(0x12);
-    LCD_1IN14_SendCommand(0xC4);  //VDV Set
-    LCD_1IN14_SendData_8Bit(0x20);
-
-    LCD_1IN14_SendCommand(0xC6);  //Frame Rate Control in Normal Mode
-    LCD_1IN14_SendData_8Bit(0x0F);
-    
-    LCD_1IN14_SendCommand(0xD0);  // Power Control 1
-    LCD_1IN14_SendData_8Bit(0xA4);
-    LCD_1IN14_SendData_8Bit(0xA1);
-
-    LCD_1IN14_SendCommand(0xE0);  //Positive Voltage Gamma Control
-    LCD_1IN14_SendData_8Bit(0xD0);
-    LCD_1IN14_SendData_8Bit(0x04);
-    LCD_1IN14_SendData_8Bit(0x0D);
-    LCD_1IN14_SendData_8Bit(0x11);
-    LCD_1IN14_SendData_8Bit(0x13);
-    LCD_1IN14_SendData_8Bit(0x2B);
-    LCD_1IN14_SendData_8Bit(0x3F);
-    LCD_1IN14_SendData_8Bit(0x54);
-    LCD_1IN14_SendData_8Bit(0x4C);
-    LCD_1IN14_SendData_8Bit(0x18);
-    LCD_1IN14_SendData_8Bit(0x0D);
-    LCD_1IN14_SendData_8Bit(0x0B);
-    LCD_1IN14_SendData_8Bit(0x1F);
-    LCD_1IN14_SendData_8Bit(0x23);
-
-    LCD_1IN14_SendCommand(0xE1);  //Negative Voltage Gamma Control
-    LCD_1IN14_SendData_8Bit(0xD0);
-    LCD_1IN14_SendData_8Bit(0x04);
-    LCD_1IN14_SendData_8Bit(0x0C);
-    LCD_1IN14_SendData_8Bit(0x11);
-    LCD_1IN14_SendData_8Bit(0x13);
-    LCD_1IN14_SendData_8Bit(0x2C);
-    LCD_1IN14_SendData_8Bit(0x3F);
-    LCD_1IN14_SendData_8Bit(0x44);
-    LCD_1IN14_SendData_8Bit(0x51);
-    LCD_1IN14_SendData_8Bit(0x2F);
-    LCD_1IN14_SendData_8Bit(0x1F);
-    LCD_1IN14_SendData_8Bit(0x1F);
-    LCD_1IN14_SendData_8Bit(0x20);
-    LCD_1IN14_SendData_8Bit(0x23);
-
-    LCD_1IN14_SendCommand(0x21);  //Display Inversion On
-
-    LCD_1IN14_SendCommand(0x11);  //Sleep Out
-
-    LCD_1IN14_SendCommand(0x29);  //Display On
-}
-// procedura init z innej biblioteki
-{
-    ST77XX_SWRESET,   ST_CMD_DELAY, //  1: Software reset, no args, w/delay
-      150,                          //     ~150 ms delay
-    ST77XX_SLPOUT ,   ST_CMD_DELAY, //  2: Out of sleep mode, no args, w/delay
-      10,                          //      10 ms delay
-    ST77XX_COLMOD , 1+ST_CMD_DELAY, //  3: Set color mode, 1 arg + delay:
-      0x55,                         //     16-bit color
-      10,                           //     10 ms delay
-    ST77XX_MADCTL , 1,              //  4: Mem access ctrl (directions), 1 arg:
-      0x08,                         //     Row/col addr, bottom-top refresh
-    ST77XX_CASET  , 4,              //  5: Column addr set, 4 args, no delay:
-      0x00,
-      0,        //     XSTART = 0
-      0,
-      240,  //     XEND = 240
-    ST77XX_RASET  , 4,              //  6: Row addr set, 4 args, no delay:
-      0x00,
-      0,             //     YSTART = 0
-      320>>8,
-      320&0xFF,  //     YEND = 320
-    ST77XX_INVON  ,   ST_CMD_DELAY,  //  7: hack
-      10,
-    ST77XX_NORON  ,   ST_CMD_DELAY, //  8: Normal display on, no args, w/delay
-      10,                           //     10 ms delay
-    ST77XX_DISPON ,   ST_CMD_DELAY, //  9: Main screen turn on, no args, delay
-      10 };  
-#define ST_CMD_DELAY 0x80 // special signifier for command lists
 
 #define ST77XX_NOP      0x00
 #define ST77XX_SWRESET  0x01
@@ -222,25 +70,31 @@ static void LCD_1IN14_SetAttributes(UBYTE Scan_dir)
 #define ST77XX_RDID2    0xDB
 #define ST77XX_RDID3    0xDC
 #define ST77XX_RDID4    0xDD
-*/
+
 
 namespace sgl
 {
 namespace st8779vw
 {
 
+  enum class ScanDir: uint8_t
+  {
+    Horizontal = 0x00,
+    Vertical = 0x01
+  };
+
 class SGL_ST8779VW: public SGL
 {
 public:
-    SGL_ST8779VW(uint16_t x, uint16_t y, uint MOSI, uint MISO, uint CLK, uint CS, uint DC, uint RST) : SGL(x, y), MOSI_(MOSI), MISO_(MISO), CLK_(CLK), CS_(CS), DC_(DC), RST_(RST) {}
-    SGL_ST8779VW(uint16_t x, uint16_t y, uint MOSI, uint MISO, uint CLK, uint CS, uint DC, uint RST, uint BLK) : SGL(x, y), MOSI_(MOSI), MISO_(MISO), CLK_(CLK), CS_(CS), DC_(DC), RST_(RST), BLK_(BLK) {}
-    SGL_ST8779VW(uint16_t x, uint16_t y, spi_inst_t* spi, uint CS, uint DC, uint RST) : SGL(x, y), spi_(spi), CS_(CS), DC_(DC), RST_(RST) {}
-    SGL_ST8779VW(uint16_t x, uint16_t y, spi_inst_t* spi, uint CS, uint DC, uint RST, uint BLK) : SGL(x, y), spi_(spi), CS_(CS), DC_(DC), RST_(RST), BLK_(BLK) {}
-    void drawPixel(uint16_t x, uint16_t y, const uint16_t color = WHITE, const Mode mode = Mode::pixelAND) override;
-    void init();
+    SGL_ST8779VW(uint16_t w, uint16_t h, uint MOSI, uint MISO, uint CLK, uint CS, uint DC, uint RST) : SGL(w, h), MOSI_(MOSI), MISO_(MISO), CLK_(CLK), CS_(CS), DC_(DC), RST_(RST) {}
+    SGL_ST8779VW(uint16_t w, uint16_t h, uint MOSI, uint MISO, uint CLK, uint CS, uint DC, uint RST, uint BLK) : SGL(w, h), MOSI_(MOSI), MISO_(MISO), CLK_(CLK), CS_(CS), DC_(DC), RST_(RST), BLK_(BLK) {}
+    SGL_ST8779VW(uint16_t w, uint16_t h, spi_inst_t* spi, uint CS, uint DC, uint RST) : SGL(w, h), spi_(spi), CS_(CS), DC_(DC), RST_(RST) {}
+    SGL_ST8779VW(uint16_t w, uint16_t h, spi_inst_t* spi, uint CS, uint DC, uint RST, uint BLK) : SGL(w, h), spi_(spi), CS_(CS), DC_(DC), RST_(RST), BLK_(BLK) {}
+    void drawPixel(uint16_t w, uint16_t h, const uint16_t color = WHITE, const Mode mode = Mode::pixelAND) override;
+    void init(ScanDir dir = ScanDir::Horizontal);
     // backlight level, 0 means power off, 100 means power max, 1-99 by PWM Control
     //void setBackLight(uint8_t level);
-    //void reset();
+    void reset();
 protected:
     spi_inst_t* spi_ = nullptr;
     uint MOSI_  = 0;
@@ -250,9 +104,11 @@ protected:
     uint DC_    = 0;
     uint RST_   = 0;
     uint BLK_   = 0;
-    //void sendCommand(uint8_t command);
-    //void sendData_8bit(uint8_t data);
-    //void sendData_16bit(uint16_t data);
+    void setPosition(ScanDir dir);
+    void initReg();
+    void sendCommand8(uint8_t command);
+    void sendData8(uint8_t data);
+    void sendData16(uint16_t data);
     //void startDataWrite();
     //void startCommandWrite();
     //void stopDataWrite();
--- drivers/ST7789VW/src/sgl_st8779vw.cpp
+++ drivers/ST7789VW/src/sgl_st8779vw.cpp
@@ -10,9 +10,134 @@ void SGL_ST8779VW::drawPixel(uint16_t x, uint16_t y, const uint16_t color, const
     ;
 }
 
-void SGL_ST8779VW::init()
+void SGL_ST8779VW::init(ScanDir dir)
 {
-    ;
+    reset();
+    setPosition(dir);
+    initReg();
+}
+
+void SGL_ST8779VW::setPosition(ScanDir dir)
+{
+    if(dir == ScanDir::Horizontal)
+    {
+        sendCommand8(ST77XX_MADCTL)
+        sendData8(0X70);
+    }
+    else
+    {
+        std::swap(width_, height_);
+        sendCommand8(ST77XX_MADCTL)
+        sendData8(0X00);
+    }
+}
+
+void SGL_ST8779VW::initReg()
+{
+    sendCommand8(0x3A);
+    sendData8(0x05);
+
+    sendCommand8(0xB2);
+    sendData8(0x0C);
+    sendData8(0x0C);
+    sendData8(0x00);
+    sendData8(0x33);
+    sendData8(0x33);
+
+    sendCommand8(0xB7);  //Gate Control
+    sendData8(0x35);
+
+    sendCommand8(0xBB);  //VCOM Setting
+    sendData8(0x19);
+
+    sendCommand8(0xC0); //LCM Control     
+    sendData8(0x2C);
+
+    sendCommand8(0xC2);  //VDV and VRH Command Enable
+    sendData8(0x01);
+    sendCommand8(0xC3);  //VRH Set
+    sendData8(0x12);
+    sendCommand8(0xC4);  //VDV Set
+    sendData8(0x20);
+
+    sendCommand8(0xC6);  //Frame Rate Control in Normal Mode
+    sendData8(0x0F);
+    
+    sendCommand8(0xD0);  // Power Control 1
+    sendData8(0xA4);
+    sendData8(0xA1);
+
+    sendCommand8(0xE0);  //Positive Voltage Gamma Control
+    sendData8(0xD0);
+    sendData8(0x04);
+    sendData8(0x0D);
+    sendData8(0x11);
+    sendData8(0x13);
+    sendData8(0x2B);
+    sendData8(0x3F);
+    sendData8(0x54);
+    sendData8(0x4C);
+    sendData8(0x18);
+    sendData8(0x0D);
+    sendData8(0x0B);
+    sendData8(0x1F);
+    sendData8(0x23);
+
+    sendCommand8(0xE1);  //Negative Voltage Gamma Control
+    sendData8(0xD0);
+    sendData8(0x04);
+    sendData8(0x0C);
+    sendData8(0x11);
+    sendData8(0x13);
+    sendData8(0x2C);
+    sendData8(0x3F);
+    sendData8(0x44);
+    sendData8(0x51);
+    sendData8(0x2F);
+    sendData8(0x1F);
+    sendData8(0x1F);
+    sendData8(0x20);
+    sendData8(0x23);
+
+    sendCommand8(0x21);  //Display Inversion On
+
+    sendCommand8(0x11);  //Sleep Out
+
+    sendCommand8(0x29);  //Display On
+}
+
+void SGL_ST8779VW::reset()
+{
+    gpio_put(RST_, 1);
+    sleep_ms(100);
+    gpio_put(RST_, 0);
+    sleep_ms(100);
+    gpio_put(RST_, 1);
+    sleep_ms(100);
+}
+
+void SGL_ST8779VW::sendCommand8(uint8_t command)
+{
+    gpio_put(DC_, 0);
+    gpio_put(CS_, 0);
+    spi_write_blocking(spi_, &command, 1);
+    gpio_put(CS_, 1);
+}
+
+void SGL_ST8779VW::sendData8(uint8_t data)
+{
+    gpio_put(DC_, 1);
+    gpio_put(CS_, 0);
+    spi_write_blocking(spi_, &data, 1);
+    pio_put(CS_, 1);
+}
+void SGL_ST8779VW::sendData16(uint16_t data)
+{
+    gpio_put(DC_, 1);
+    gpio_put(CS_, 0);
+    spi_write_blocking(spi_, &((data >> 8) & 0xFF), 1);
+    spi_write_blocking(spi_, &(data & 0xFF), 1);
+    pio_put(CS_, 1);
 }
 
 }
